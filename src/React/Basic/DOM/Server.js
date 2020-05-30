46c55722dbd0327f92eeb3761152155ce9ebd2a1
Derrick Beining <derrick.beining@gmail.com>
2020-05-30 12:38:17 -0400

    Add React.DOM.Server renderTo{String,StaticMarkup}

---
+++ src/React/Basic/DOM/Server.js
@@ -0,0 +1,7 @@
+"use strict"
+
+var ReactDOMServer = require("react-dom/server")
+
+exports.renderToString = ReactDOMServer.renderToString
+
+exports.renderToStaticMarkup = ReactDOMServer.renderToStaticMarkup
